3fdc01dbb6428c2a256fe3f3ca875245ad02f361
Egor18032019 <goro4@mail.ru>
2021-08-13 10:08:16 +0500

    Создали сервер

--- src/Server.java
+++ src/Server.java
@@ -1,23 +1,44 @@
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8000);
-
         int count = 0;
         boolean serverWork = true;
         while (serverWork) {
+//TODO Придумать как сделать serverWork = false;
             Socket clientSocket = serverSocket.accept(); // получение от клиента
             count++;
-            System.out.println("Cleint accepter: " + count);
+            System.out.println("Client accepted: " + count);
             // работает пока клиент не подключиться -> долгая
             // clientSocket.getOutputStream().write(3);// по одному байту
-            clientSocket.getOutputStream().write("<h2>Hello bro</h2>".getBytes());
+            // clientSocket.getOutputStream().write(("HTTP/1.0 200 OK\n" +
+            //  "Content-type:text/html\n"+
+            //  "\r\n"+
+            // "<h1>Hello bro</h1>\n").getBytes());
+
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+           String request = reader.readLine();
+
             OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
-            writer.write("<h1>How you ?" + count + " </h1>");
+            String answer = "<h1>Hello bro</h1>" +
+                    "<p>You " + count + "</p>" +
+                    "<p>You message:" + request.length()+ "</p>";
+            System.out.println(answer);
+            writer.write("HTTP/1.0 200 OK\n" +
+                    "Content-type:text/html\n" +
+                    "Content-Length:" + answer.length() + "\n" +
+                    "\r\n" +
+                    answer);
+
             writer.flush();
             writer.close();
+            reader.close();
 
             clientSocket.close();// закрываем именно открытый клинетом соккет
         }
